6849628fb5aff540544c9327a4a764cce8d0b3d2
Luis Armando <luis.anescu@iisviolamarchesini.edu.it>
2024-10-30 08:47:26 +0100

    Finito es 11

---
+++ 5/socket/es11Server.c
@@ -0,0 +1,62 @@
+/*
+Esercizio 11
+Scrivere il codice in C, di un applicazione Socket CLIENT-SERVER in cui il server riceve in input un
+vettore di numeri e dopo aver effettuato gli eventuali ed opportuni controlli (se necessari), rispedisce al
+Client il conteggio dei numeri pari e dei numeri dispari contenuti nel vettore.
+*/
+#include <stdio.h>
+#include <stdlib.h>     //per utilizzo di certe funzioni:htonl,rand,....
+#include <sys/socket.h> //funz. accept+bind+listen
+#include <sys/types.h>  //funz. accept
+#include <netinet/in.h> //definiscono la struttura degli indirizzi
+#include <string.h>     //funz. stringhe
+#include <errno.h>      //gestioni errori connessione
+#include <ctype.h>      //bind
+#include <unistd.h>     // file header che consente l'accesso alle API dello standard POSIX
+
+#define DIM 60
+#define SERVERPORT 1313
+void ContaPariDispari(int arr[], int len, char strRis[])
+{
+    int nPari = 0, nDispari = 0;
+    for (int i = 0; i < len; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            nPari++;
+        }
+        else
+        {
+            nDispari++;
+        }
+    }
+    sprintf(strRis, "ci sono %d numeri pari e %d dispari\n",nPari,nDispari);
+}
+int main(int argc, char **argv)
+{
+    struct sockaddr_in servizio, addr_remoto; // record con i dati del server e del client
+    int socketfd, soa, fromlen = sizeof(servizio);
+    int len;
+    char stringaRis[DIM];
+    servizio.sin_family = AF_INET;
+    servizio.sin_addr.s_addr = htonl(INADDR_ANY);
+    servizio.sin_port = htons(SERVERPORT);
+    socketfd = socket(AF_INET, SOCK_STREAM, 0);
+    bind(socketfd, (struct sockaddr *)&servizio, sizeof(servizio));
+    // poniamo il server in ascolto delle richieste dei client
+    listen(socketfd, 10);
+    for (;;)
+    {
+        printf("\n\nServer in ascolto... \n");
+        fflush(stdout);
+        soa = accept(socketfd, (struct sockaddr *)&addr_remoto, &fromlen);
+        // legge dal client
+        read(soa, &len, sizeof(len));
+        int array[len];
+        read(soa, array, sizeof(array));
+        ContaPariDispari(array, len,stringaRis);
+        write(soa, stringaRis, sizeof(stringaRis));
+        close(soa);
+    }
+    return 0;
+}
