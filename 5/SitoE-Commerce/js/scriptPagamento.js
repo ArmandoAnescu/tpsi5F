3a35b8a2b23d0f8555a0be7baf56537c823cd881
Armando <luis.anescu@iisviolamarchesini.edu.it>
2025-02-22 19:44:12 +0100

    mod

--- 5/SitoE-Commerce/js/scriptPagamento.js
+++ 5/SitoE-Commerce/js/scriptPagamento.js
@@ -1,5 +1,5 @@
 function loadJSON() {
-    fetch('index.json') // URL del file JSON
+    fetch('pagamento.json') // URL del file JSON
         .then(response => {
             if (!response.ok) {
                 throw new Error('Errore nel caricamento del file JSON');//dico che c'è stato un errore
@@ -29,6 +29,34 @@ function loadPage(jsonData) {
         listItem.appendChild(link);
         navbarLinks.appendChild(listItem);
     });
+    const paymentMethods = document.getElementById('payment-method');
+    if (jsonData.metodoPagamento && Array.isArray(jsonData.metodoPagamento)) {
+        jsonData.metodoPagamento.forEach(metodo => {
+            const option = document.createElement('option');
+            option.value = metodo;
+            option.textContent = metodo;
+            paymentMethods.appendChild(option);
+        });
+    }
+    // Titoli (h2)
+document.getElementById("transaction").textContent = jsonData.transaction;
+document.getElementById("personalDetails").textContent = jsonData.personalDetails;
+
+// Label dei campi di pagamento
+document.getElementById("paymentMethod").textContent = jsonData.transaction;
+document.getElementById("card-number-label").textContent = jsonData.cardNumLabel;
+document.getElementById("expDate").textContent = jsonData.expDate;
+document.getElementById("secCode").textContent = jsonData.secCode;
+
+// Label dei dettagli personali
+document.getElementById("names-label").textContent = jsonData.namesLabel;
+document.getElementById("addrCity").textContent = jsonData.addrCity;
+document.getElementById("postal-code-label").textContent = jsonData.pcLabel;
+document.getElementById("countryLabel").textContent = jsonData.countryLabel;
+document.getElementById("phoneNumber").textContent = jsonData.phoneNumber;
+
+// Pulsante di invio
+document.getElementById("submit").textContent = jsonData.submit;
 
     document.getElementById('footerText').textContent = jsonData.footer.text;
 }
