cd858c314e617d0132550ad2a01b66e79a6306fe
Armando <luis.anescu@iisviolamarchesini.edu.it>
2025-03-17 17:21:05 +0100

    aggiustato caricamento json

--- 5/E-Commerce-PHP/footer.php
+++ 5/E-Commerce-PHP/footer.php
@@ -10,7 +10,6 @@
 <script src="js/bootstrap.bundle.min.js"></script>
 <?php
 $current_page = basename($_SERVER['PHP_SELF']); // Ottiene solo il nome del file
-
 switch ($current_page) {
     case "index.php": ?>
         <script src="js/script.js"></script>
@@ -23,6 +22,7 @@ switch ($current_page) {
     case "carrello.php": ?>
         <script src="js/scriptCarrello.js"></script>
     <?php
+        break;
     case "archivio.php": ?>
         <script src="js/scriptArchivio.js"></script>
 <?php
--- 5/E-Commerce-PHP/js/scriptCarrello.js
+++ 5/E-Commerce-PHP/js/scriptCarrello.js
@@ -15,7 +15,7 @@ function loadJSON() {
       console.error('Errore:', error);
     });
 }
-let totale = 0;
+
 function LoadPage(jsonData) {
   const icona = document.getElementById('nav-brand');
   icona.innerHTML = jsonData.logo;
@@ -30,63 +30,10 @@ function LoadPage(jsonData) {
     listItem.appendChild(link);
     navbarLinks.appendChild(listItem);
   });
-  document.getElementById('pageTitle').textContent = jsonData.cartTitle;
+  document.getElementById('cartTitle').textContent = jsonData.cartTitle;
   document.getElementById('empty-cart').textContent = jsonData.emptyCart;
   document.getElementById('acquista').textContent = jsonData.acquista;
   document.getElementById('footerText').textContent = jsonData.footer.text;
-}
-// Funzione per caricare dinamicamente i prodotti
-function caricaProdotti(prodotti) {
-  const container = document.getElementById("cart-container");
-  let carrello = JSON.parse(localStorage.getItem("carrello"));
-  if (carrello) {
-    carrello.forEach((carrelloProdotto) => {
-      prodotti.forEach(prodotto => {
-        if (carrelloProdotto.id === prodotto.id) {
-          const card = document.createElement("div");
-          // Aggiungi un'immagine di default (se non disponibile nel JSON)
-          card.innerHTML = `
-            <div class="card mb-3" style="max-width: 540px;">
-              <div class="row g-0">
-                <div class="col-md-4">
-                  <img src="${prodotto.immagine}" class="img-fluid rounded-start" alt="${prodotto.nome}">
-                </div>
-                <div class="col-md-8">
-                  <div class="card-body">
-                    <h5 class="card-title">${prodotto.nome}</h5>
-                    <p class="card-text price">€${prodotto.prezzo}</p>
-                    <p class="card-text">quantità: ${carrelloProdotto.quantita}</p>
-                    
-                    <a href="paginaProdotto.html?id=${prodotto.id}" class="btn btn-primary">Vedi prodotto</a>
-                    <a id="remove-item" class="btn btn-danger" style="margin-left: 10px;" onclick="rimuoviDalCarrello('${prodotto.id}')">Rimuovi prodotto</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          `;
-
-          let prezzo = prodotto.prezzo.trim().replace(',', '.');  // Rimuove eventuali spazi vuoti
-          let quantita = carrelloProdotto.quantita.trim();  // Fa lo stesso per la quantità
-          // Verifica se sono numeri validi
-          if (!isNaN(prezzo) && !isNaN(quantita)) {
-            totale = totale + Number(prezzo) * Number(quantita);
-            totale = parseFloat(totale.toFixed(2));
-          }
-          // Aggiungiamo la card al contenitore
-          container.appendChild(card);
-        }//end
-
-      });
-    });
-    document.getElementById('price').innerHTML = `totale: <span class="price">€${totale}</span>`;
-  } else {
-    let alert = document.createElement('div');
-    alert.innerHTML = `<div class="alert alert-secondary" role="alert">
-    Il tuo carrello è vuoto.
-                      </div>`
-    container.appendChild(alert);
-  }
-
 }
 function rimuoviDalCarrello(productId) {
   let carrello = JSON.parse(localStorage.getItem("carrello")) || [];
