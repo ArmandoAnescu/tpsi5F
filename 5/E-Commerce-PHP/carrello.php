576a236ab9b042fb1c1ace90168b002238238df4
ArmandoAnescu <121225784+ArmandoAnescu@users.noreply.github.com>
2025-03-19 10:19:48 +0100

    Update carrello.php

--- 5/E-Commerce-PHP/carrello.php
+++ 5/E-Commerce-PHP/carrello.php
@@ -28,8 +28,10 @@ $prodotti = OttieniProdotti();
                                                         <h5 class="card-title"><?= $prodotto['nome'] ?></h5>
                                                         <p class="card-text price">€<?= $prodotto['prezzo'] ?></p>
                                                         <p class="card-text">quantità: <?= $item['quantita'] ?></p>
-                                                        <a href="prodotto.php?id=<?= $prodotto['id'] ?>" class="btn btn-primary">Vedi prodotto</a>
-                                                        <a id="remove-item" class="btn btn-danger" style="margin-left: 10px;">Rimuovi prodotto</a>
+                                                        <div class="btn-group">
+                                                            <a href="prodotto.php?id=<?= $prodotto['id'] ?>" class="btn btn-primary">Vedi prodotto</a>
+                                                            <a id="remove-item" class="btn btn-danger">Rimuovi prodotto</a>
+                                                        </div>
                                                     </div>
                                                 </div>
                                             </div>
